e7f959f78aea31ae2229759966adfed6e7844db7
alokozzay <moyzeekornev@gmail.com>
2025-11-05 01:07:05 +0100

    v1.1

--- client/src/layouts/header/Header.jsx
+++ client/src/layouts/header/Header.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import Logo from "../../assets/Logo.png";
+import { Link } from "react-router-dom";
+import style from "./style.module.css";
 
 export default function Header() {
-    return <div>Header</div>;
+    return (
+        <div className={style.navbar}>
+            <div className={`container ${style.container}`}>
+                <Link to="/">
+                    <img src={Logo} alt="Logo" className={style.logo} />
+                </Link>
+                <ul className={style.menu}>
+                    <li>
+                        <Link to="/profile">Profile</Link>
+                    </li>
+                    <li>
+                        <Link to="/create">Create post</Link>
+                    </li>
+                    <li>
+                        <Link to="/authors">Authors</Link>
+                    </li>
+                    <li>
+                        <Link to="/logout">Logout</Link>
+                    </li>
+                </ul>
+                <button className={style.button_toggle}>x</button>
+            </div>
+        </div>
+    );
 }
